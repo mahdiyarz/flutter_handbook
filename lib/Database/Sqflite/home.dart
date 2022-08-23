0ebae6bbe54a90bda098ab7c6d752958675fe344
mahDyarZ <m.arbabzi@gmail.com>
2022-08-24 00:35:52 +0430

    Change the hole thing and convert to a sample app

---
+++ lib/Database/Sqflite/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+
+class NoteAppWithSqf extends StatefulWidget {
+  const NoteAppWithSqf({Key? key}) : super(key: key);
+
+  @override
+  State<NoteAppWithSqf> createState() => _NoteAppWithSqfState();
+}
+
+class _NoteAppWithSqfState extends State<NoteAppWithSqf> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import './Database/Sqflite/home.dart';
+
+void main() {
+  runApp(const NoteAppWithSqf());
+}
